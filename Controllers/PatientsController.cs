1cc969660cf93641b79700c50d6878442e95c210
Hallae <mr.tabby@hotmail.com>
2024-04-13 23:44:13 +0300

    Added: Http methods /Dapper

--- Controllers/PatientsController.cs
+++ Controllers/PatientsController.cs
@@ -1,13 +1,63 @@
-﻿using Microsoft.AspNetCore.Http;
+﻿using Dapper;
+using DapperCrudApi.Models;
+using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Infrastructure;
 
 namespace DapperCrudApi.Controllers
 {
     [Route("api/[controller]")]
     [ApiController]
 
-    private readonly Data
+    
     public class PatientsController : ControllerBase
     {
+        private readonly DatabaseContext _dbContext; 
+        public PatientsController(DatabaseContext dbcontext)
+        {
+            _dbContext = dbcontext;
+        }
+
+
+        [HttpGet]
+        public ActionResult<List<Patients>> Get()
+        {
+            using (var connection = _dbContext.Connection)
+            {
+                connection.Open();
+                var patients = connection.Query<Patients>("SELECT * FROM Patients").ToList();
+                return Ok(patients);
+            }
+        }
+
+        [HttpGet("{id}")]
+        public ActionResult<Patients> GetPatient(int id)
+        {
+            using (var connection = _dbContext.Connection)
+            {
+                connection.Open();
+                var patient = connection.QueryFirstOrDefault<Patients>("SELECT * FROM Patients WHERE id = @Id", new { Id = id });
+                if (patient == null)
+                {
+                    return NotFound();
+                }
+                return Ok(patient);
+            }
+        }
+
+
+        [HttpPost]
+        public ActionResult<Patients> CreatePatient(Patients patient)
+        {
+            using (var connection = _dbContext.Connection)
+            {
+                connection.Open();
+                var query = "INSERT INTO Patients (age, FirstName, LastName, CauseofDeath, Address, Nationality, TimeofDeath) VALUES (@Age, @FirstName, @LastName, @CauseofDeath, @Address, @Nationality, @TimeofDeath); SELECT LASTVAL();";
+                var patientId = connection.QuerySingle<int>(query, patient);
+                patient.id = patientId;
+                return CreatedAtAction(nameof(GetPatient), new { id = patient.id }, patient);
+            }
+        }
+
     }
 }
---
+++ Models/DatabaseContext.cs
@@ -0,0 +1,24 @@
+﻿using Npgsql;
+using System.Data;
+
+namespace DapperCrudApi.Models
+{
+    public class DatabaseContext
+    {
+
+        private readonly string _connectionString;
+
+        public DatabaseContext(IConfiguration configuration)
+        {
+            _connectionString = configuration.GetConnectionString("DefaultConnection");
+        }
+
+        public IDbConnection Connection
+        {
+            get
+            {
+                return new NpgsqlConnection(_connectionString);
+            }
+        }
+    }
+}
