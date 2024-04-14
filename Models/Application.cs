04baa05c16e76c23fb0327aa033ff39bf1e21576
Hallae <mr.tabby@hotmail.com>
2024-04-15 01:11:29 +0300

    Added: Application Model

---
+++ Models/Application.cs
@@ -0,0 +1,23 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace DapperCrudApi.Models
+{
+    public class Application
+    {
+        [Key]
+        [Required]// This attribute specifies that the Id property is the primary key
+        public Guid id { get; set; } = Guid.NewGuid();
+        // Make the property settable and initialize with a new GUID
+        public Guid author { get; set; } = Guid.NewGuid();
+
+
+        [Required]
+        public string activity { get; set; }
+        [Required]
+        public string name { get; set; }
+        public string description { get; set; }
+        public string outline { get; set; }
+        public bool IsSubmitted { get; set; }
+        public DateTime SubmissionDate { get; set; }
+    }
+}
