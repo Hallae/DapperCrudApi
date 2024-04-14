08933b3f7560217712945df0fc583324a7457f89
Hallae <mr.tabby@hotmail.com>
2024-04-15 01:33:53 +0300

    Added:Application connection string/app settings

--- Models/DatabaseContext.cs
+++ Models/DatabaseContext.cs
@@ -1,23 +1,38 @@
 ﻿using Npgsql;
 using System.Data;
+using Microsoft.Extensions.Configuration;
 
 namespace DapperCrudApi.Models
 {
+    //<summary>
+    // Database Connections Strings
+    //</summary>
+    
     public class DatabaseContext
     {
-
         private readonly string _connectionString;
+        private readonly string _connectionStringApp;
 
         public DatabaseContext(IConfiguration configuration)
         {
             _connectionString = configuration.GetConnectionString("DefaultConnection");
+            _connectionStringApp = configuration.GetConnectionString("ApplicationConnection");
         }
 
-        public IDbConnection Connection
+        //Responsible for the Patients postgres connectionstring
+        public IDbConnection DefaultConnection
         {
             get
             {
                 return new NpgsqlConnection(_connectionString);
+            }
+        }
+        //Responsible for the Application postgres connectionstring
+        public IDbConnection ApplicationConnection
+        {
+            get
+            {
+                return new NpgsqlConnection(_connectionStringApp);
             }
         }
     }
